8fbb8bb144703f2b47da9e894c098c2fa179c7a0
cvs-fast-export <cvs-fast-export>
2005-01-07 11:13:12 +0000

    Synthetic commit for incomplete tag ikvm

--- classpath/ikvm/lang/ByteArrayHack.java
+++
@@ -1,9 +0,0 @@
-package ikvm.lang;
-
-public final class ByteArrayHack
-{
-    private ByteArrayHack() {}
-
-    public static native cli.System.Byte[] cast(byte[] b);
-    public static native byte[] cast(cli.System.Byte[] b);
-}
